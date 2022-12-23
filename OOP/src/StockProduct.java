eb4d3d6c78453e256740277ea41a84a5d3e74c64
Lagedane <cyjae96@gmail.com>
2022-12-23 15:33:03 +0700

    WorkShop 7 [23-12-2022]

--- OOP/src/StockProduct.java
+++ OOP/src/StockProduct.java
@@ -1,6 +1,6 @@
 import java.util.*;
 import java.text.*;
- 
+  
 public class StockProduct {
 
 	public static void main(String[] args) {
@@ -10,12 +10,23 @@ public class StockProduct {
 		
 		for(int i = 0; i < input.length; i++) {
 			input[i] = new Product();
+			// input ID
 			System.out.print("Input product Id     : ");
 			input[i].setId(scan.next());
+			// input UNIT
 			System.out.print("Input product Unit   : ");
 			input[i].setUnit(scan.nextInt());
+			while(input[i].getUnit() <= 0) {
+				System.out.print("Input product Unit, again   : ");
+				input[i].setUnit(scan.nextInt());
+			} // end while
+			// input PRICE
 			System.out.print("Input product Price  : ");
 			input[i].setPrice(scan.nextDouble());
+			while(input[i].getPrice() <= 0) {
+				System.out.print("Input product Price  : ");
+				input[i].setPrice(scan.nextDouble());
+			} // end while
 			System.out.println();
 		} // end for
 		
